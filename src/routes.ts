5f24217b296ad367c02b91a2d0d1aec79e30ac1c
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-10-23 18:08:42 -0300

    added: style table.

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 import fs from 'fs';
 
 import PDFPrinter from 'pdfmake';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { TableCell, TDocumentDefinitions } from 'pdfmake/interfaces';
 
 import { prismaClient } from './database/prismaClient';
 
@@ -30,6 +30,18 @@ routes.get('/products/report', async (request: Request, response: Response) => {
 
   const body = [];
 
+  const columnsTitle: TableCell[] = [
+    { text: 'ID', style: 'columnsTitle' },
+    { text: 'Descrição', style: 'columnsTitle' },
+    { text: 'Preço', style: 'columnsTitle' },
+    { text: 'Quantidade', style: 'columnsTitle' },
+  ];
+
+  const columnsBody = new Array();
+
+  columnsTitle.forEach(column => columnsBody.push(column));
+  body.push(columnsBody);
+
   for await (let product of products) {
     const rows = new Array();
     rows.push(product.id);
@@ -44,11 +56,39 @@ routes.get('/products/report', async (request: Request, response: Response) => {
     defaultStyle: { font: 'Helvetica' },
     content: [
       {
+        columns: [
+          {
+            text: 'Relatório de Produtos',
+            style: 'header',
+          },
+          { text: '23/10/2021 17:54\n\n', style: 'header' },
+        ],
+      },
+      {
         table: {
-          body: [['ID', 'Descrição', 'Preço', 'Quantidade'], ...body],
+          heights: function (row) {
+            return 30;
+          },
+          widths: [250, 'auto', 50, 'auto'],
+          body,
         },
       },
     ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        alignment: 'center',
+      },
+      columnsTitle: {
+        fontSize: 15,
+        bold: true,
+        fillColor: '#7159c1',
+        color: '#FFF',
+        alignment: 'center',
+        margin: 4,
+      },
+    },
   };
 
   const pdfDoc = printer.createPdfKitDocument(docDefefinitions);
