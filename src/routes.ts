7fa5b1096c176c497114e4bb4ef0eae29f65093d
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-10-23 17:28:03 -0300

    creating a simple pdf.

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from 'express';
+import fs from 'fs';
 
-import {prismaClient} from './database/prismaClient';
+import PDFPrinter from 'pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+
+import { prismaClient } from './database/prismaClient';
 
 const routes = Router();
 
@@ -10,4 +14,34 @@ routes.get('/products', async (request: Request, response: Response) => {
   return response.json(products);
 });
 
-export {routes};
+routes.get('/products/report', (request: Request, response: Response) => {
+  const fonts = {
+    Helvetica: {
+      normal: 'Helvetica',
+      bold: 'Helvetica-Bold',
+      italics: 'Helvetica-Oblique',
+      bolditalics: 'Helvetica-BoldOblique',
+    },
+  };
+
+  const printer = new PDFPrinter(fonts);
+
+  const docDefefinitions: TDocumentDefinitions = {
+    defaultStyle: { font: 'Helvetica' },
+    content: [
+      {
+        text: 'textando relatório.',
+      },
+    ],
+  };
+
+  const pdfDoc = printer.createPdfKitDocument(docDefefinitions);
+
+  pdfDoc.pipe(fs.createWriteStream('Relatorio.pdf'));
+
+  pdfDoc.end();
+
+  response.send('Relatório concluído!');
+});
+
+export { routes };
